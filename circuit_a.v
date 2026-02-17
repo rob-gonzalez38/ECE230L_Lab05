17bf19efeb211ffa7edbc5c76153c6bd42fdf0d6
Rob Gonzalez <robertgonzalez@u.boisestate.edu>
2026-02-17 09:45:16 -0700

    Uploaded files

---
+++ circuit_a.v
@@ -0,0 +1,9 @@
+module circuit_a(
+    // Declare inputs
+    // Declare Y output
+);
+
+    // Enter logic equation here
+    (C | D) & (~C | D) & (~A | ~B) & (~A | B)
+
+endmodule
---
+++ circuit_b.v
@@ -0,0 +1,9 @@
+module circuit_b(
+    // Declare inputs
+    // Declare Y output
+);
+
+    // Enter logic equation here
+    (~C & ~D) | (A & B) | (B & C & ~D)
+
+endmodule
